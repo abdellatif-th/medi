01d77d109d1e43b6a354d5c94e053fb52124f150
abdellatif99.tahiri <abdellatif99.tahiri@gmail.com>
2025-08-12 12:54:08 +0100

    added logo to application

--- resources/js/Components/PhishingForm.jsx
+++ resources/js/Components/PhishingForm.jsx
@@ -86,7 +86,6 @@ const [generated, setGenerated] = useState("");
   }
 };
 
-
   return (
     <div className="max-w-2xl mx-auto p-4 bg-white dark:bg-gray-800 rounded shadow mt-8">
         <h2 className="text-xl font-bold mb-4 text-white">Phishing Email Generator</h2>
